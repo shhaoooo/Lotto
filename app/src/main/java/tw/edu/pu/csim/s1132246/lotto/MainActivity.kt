813f327d76af3198fa75b450cd7d024ae6426770
shhaoooo <asd0965104898@gmail.com>
2025-09-15 22:56:04 +0800

    第四次commit (垂直排列)

--- app/src/main/java/tw/edu/pu/csim/s1132246/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1132246/lotto/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -33,14 +34,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Play(modifier: Modifier = Modifier) {
     var lucky = (1..100).random()
-    Text(
-        text = "樂透數字(1-100)為 $lucky",
-        modifier = modifier
-    )
 
-    Button(
-        onClick = { lucky = (1..100).random() }
-    ) {
-        Text("重新產生樂透碼")
+    Column(modifier = modifier) { // <-- 將 Text 和 Button 放在 Column 中
+        Text(
+            text = "樂透數字(1-100)為 $lucky"
+        )
+
+        Button(
+            onClick = { lucky = (1..100).random() }
+        ) {
+            Text("重新產生樂透碼")
+        }
     }
+
 }
